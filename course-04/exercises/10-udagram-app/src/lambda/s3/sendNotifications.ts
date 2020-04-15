cd6ec63dae9313c83fcab6e0cd125bca6b690dfc
Shuda Mo <msdtim@gmail.com>
2020-04-15 17:41:59 +0800

    WebSockets demo

--- course-04/exercises/10-udagram-app/src/lambda/s3/sendNotifications.ts
+++ course-04/exercises/10-udagram-app/src/lambda/s3/sendNotifications.ts
@@ -1,9 +1,56 @@
 import { S3Handler, S3Event } from 'aws-lambda'
 import 'source-map-support/register'
+import * as AWS  from 'aws-sdk'
+
+const connectionsTable = process.env.CONNECTIONS_TABLE
+const stage = process.env.STAGE
+const apiId = process.env.API_ID
+
+const connectionParams = {
+  apiVersion: "2018-11-29",
+  endpoint: `${apiId}.execute-api.us-east-1.amazonaws.com/${stage}`
+}
+
+const apiGateway = new AWS.ApiGatewayManagementApi(connectionParams)
+const docClient = new AWS.DynamoDB.DocumentClient()
 
 export const handler: S3Handler = async (event: S3Event) => {
-    for (const records of event.Records) {
-      const key = records.s3.object.key
-      console.log('Processing S3 item with key', key)
+  for (const records of event.Records) {
+    const key = records.s3.object.key
+    console.log('Processing S3 item with key', key)
+    const connections = await docClient.scan({
+      TableName: connectionsTable
+    }).promise()
+  
+    const payload = {
+      imageId: key
     }
+
+    for (const connection of connections.Items) {
+      const connectionId = connection.id
+      await sendMessageToClient(connectionId, payload)
+    }
+  }
+}
+
+async function sendMessageToClient(connectionId, payload) {
+  try {
+    console.log('Sending message to a connection', connectionId)
+    await apiGateway.postToConnection({
+      ConnectionId: connectionId,
+      Data: JSON.stringify(payload),
+    }).promise()
+  } catch (e) {
+    console.log('Failed to send message', JSON.stringify(e))
+    if (e.statusCode === 410) {
+      console.log('Stale connection')
+
+      await docClient.delete({
+        TableName: connectionsTable,
+        Key: {
+          id: connectionId
+        }
+      }).promise()
+    }
+  }
 }
