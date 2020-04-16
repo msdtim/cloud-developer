d4b839706f334c88a882d46ec67dcb72e91bab98
Shuda Mo <msdtim@gmail.com>
2020-04-17 00:01:27 +0800

    Impletement Auth 0

--- course-04/exercises/lesson-5/client/src/config.ts
+++ course-04/exercises/lesson-5/client/src/config.ts
@@ -1,7 +1,7 @@
-export const apiEndpoint = '...'
+export const apiEndpoint = 'https://n5846oavqk.execute-api.us-east-1.amazonaws.com/dev/'
 
 export const authConfig = {
-  domain: '...',
-  clientId: '...',
+  domain: 'dev-4j7w7l3d.auth0.com',
+  clientId: '0qcUvi3pZ333Ac9POq0igJhxxfCs8KpP',
   callbackUrl: 'http://localhost:3000/callback'
 }
