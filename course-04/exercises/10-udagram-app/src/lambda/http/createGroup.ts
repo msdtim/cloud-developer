820844f3ea2687f64e51d260bf53d0e66b4a63e0
Shuda Mo <msdtim@gmail.com>
2020-04-17 15:04:35 +0800

    refactor: Apply Port and Adaptor Arch for Group
    Split getGroups and createGroups into dataLayer and businessLogic
    Only dataLayer will interact with service provider and encapsulate actions needed by businessLogic

--- course-04/exercises/10-udagram-app/src/lambda/http/createGroup.ts
+++ course-04/exercises/10-udagram-app/src/lambda/http/createGroup.ts
@@ -1,36 +1,18 @@
 import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import 'source-map-support/register'
-import * as AWS  from 'aws-sdk'
-import * as uuid from 'uuid'
-import {getUserId} from '../../auth/utils'
 
-const docClient = new AWS.DynamoDB.DocumentClient()
-const groupsTable = process.env.GROUPS_TABLE
+import { CreateGroupRequest } from '../../requests/CreateGroupRequest'
+import { createGroup } from '../../businessLogic/groups'
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   console.log('Processing event: ', event)
 
+  const newGroup: CreateGroupRequest = JSON.parse(event.body)
   const authorization = event.headers.Authorization
   const split = authorization.split(' ')
   const jwtToken = split[1]
 
-  const userId = getUserId(jwtToken)
-
-  const itemId = uuid.v4()
-
-  const parsedBody = JSON.parse(event.body)
-
-  const newItem = {
-    id: itemId,
-    // A new line to add
-    userId: userId, // Can be abbreviated to just "userId,"
-    ...parsedBody
-  }
-
-  await docClient.put({
-    TableName: groupsTable,
-    Item: newItem
-  }).promise()
+  const newItem = await createGroup(newGroup, jwtToken)
 
   return {
     statusCode: 201,
