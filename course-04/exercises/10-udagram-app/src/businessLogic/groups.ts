99690f12a55fe0c5ae73c3759621649765adc969
Shuda Mo <msdtim@gmail.com>
2020-04-17 18:42:35 +0800

    chore: Configure Canary Deployment for createGroup function
    Canary deployment with policy Linear10PercentEvery1Minute

--- course-04/exercises/10-udagram-app/src/businessLogic/groups.ts
+++ course-04/exercises/10-udagram-app/src/businessLogic/groups.ts
@@ -24,5 +24,6 @@ export async function createGroup(
     userId: userId,
     name: createGroupRequest.name,
     description: createGroupRequest.description,
+    timestamp: new Date().toISOString()
   })
 }
